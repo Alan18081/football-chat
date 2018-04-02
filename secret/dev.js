61c4cb28c9d8dff95760f082007c003885b04811
Alex Ostapyuk <gogunov00@gmail.com>
2018-04-02 15:15:31 +0300

    las

---
+++ secret/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  facebook: {
+    clientID: '1562588197192647',
+    clientSecret: 'e6cb53b83059f5cc7cbd97c5871cf7c0'
+  },
+  google: {
+    clientID: '813216506346-a8fi6r04gn3bu6d0eoeguufd9ir4vj8i.apps.googleusercontent.com',
+    clientSecret: 'hhXWuIK681bmekrIv-ygMw2r'
+  },
+  mongodb: 'mongodb://AlexMorgan:morgan11@ds121289.mlab.com:21289/socket'
+};
